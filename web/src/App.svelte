4c5a1b30ba3194daebb8afa2b38bb5d2a66125e0
avenbreaks <dev-cosmware@gitshock.com>
2022-10-19 19:19:16 +0700

    Update: Faucet Testnet GTFX

---
+++ web/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import 'animate.css';
+  import 'bulma/css/bulma.css';
+  import Faucet from './Faucet.svelte';
+</script>
+
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400&display=swap" rel="stylesheet">
+</svelte:head>
+
+<Faucet />
